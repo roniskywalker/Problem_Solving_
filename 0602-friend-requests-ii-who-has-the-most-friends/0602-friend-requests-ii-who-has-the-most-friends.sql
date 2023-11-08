f56b23b546b0d023a53aebe3131c1fa4eea59895
Ankan Hazra <ankanhazraeducation@gmail.com>
2023-11-08 11:22:49 +0530

    Time: 507 ms (69.61%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+with cte as(
+(select requester_id as id, accepter_id
+from RequestAccepted)
+union
+(select accepter_id, requester_id
+from RequestAccepted)
+)
+
+select id, count(distinct accepter_id) as num
+from cte
+group by id
+order by num desc
+limit 1
