3d456781aaab2511f2b33ceaca92d1f732456c99
Ankan Hazra <ankanhazraeducation@gmail.com>
2024-07-25 23:48:57 +0530

    Time: 50 ms (90.5%), Space: 18.5 MB (65.16%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,3 +1,8 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        return ((3*sum((set(nums))))-sum(nums))//2
+    def singleNumber(self, nums):
+        ones = 0
+        twos = 0
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+        return ones
