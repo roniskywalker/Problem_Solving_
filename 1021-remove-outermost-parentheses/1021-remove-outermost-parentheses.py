2753223237583e4557c48ead0d0de7d2794fbe88
Ankan Hazra <ankanhazraeducation@gmail.com>
2024-12-20 18:48:42 +0530

    Time: 0 ms (100%), Space: 17.7 MB (9.52%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -4,10 +4,8 @@ class Solution:
         counter = 0
         for i in range(len(s)):
             if s[i] == "(":
-                if counter == 0:
-                    counter += 1
-                else:
-                    counter += 1
+                counter += 1
+                if counter != 1:
                     st += "("
             else:
                 counter -= 1
